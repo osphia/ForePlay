b75256e2eeee37a1a426375e15ed1e6aac5431b6
Sophia Lee <sophiaylee23@gmail.com>
2024-06-05 23:10:11 -0700

    minor fixes to get rid of unnecessary code

--- main.js
+++ main.js
@@ -188,66 +188,8 @@ export class Main extends Scene {
         this.collected_coins = 0;
 
         this.key_state = { ArrowUp: false, ArrowLeft: false, ArrowDown: false, ArrowRight: false, Enter: false };
-        this.add_key_listener();
         this.canvas = document.querySelector('#main-canvas');
         this.attach_event_listeners();
-    }
-
-    add_key_listener() {
-        window.addEventListener('keydown', (event) => {
-            switch (event.key) {
-                case 'ArrowUp':
-                    this.key_state.ArrowUp = true;
-                    event.preventDefault();
-                    break;
-                case 'ArrowLeft':
-                    this.key_state.ArrowLeft = true;
-                    event.preventDefault();
-                    break;
-                case 'ArrowDown':
-                    this.key_state.ArrowDown = true;
-                    event.preventDefault();
-                    break;
-                case 'ArrowRight':
-                    this.key_state.ArrowRight = true;
-                    event.preventDefault();
-                    break;
-                case 'Shift':
-                    this.camera_mode = (this.camera_mode === 'third_person') ? 'first_person' : 'third_person';
-                    break;
-            }
-            if (event.key === 'Enter' && !this.isEnterPressed && this.isBallStationary()) {
-                this.isEnterPressed = true;
-                this.enter_press_time = Date.now();
-                event.preventDefault();
-            }
-        });
-
-        window.addEventListener('keyup', (event) => {
-            switch (event.key) {
-                case 'ArrowUp':
-                    this.key_state.ArrowUp = false;
-                    break;
-                case 'ArrowLeft':
-                    this.key_state.ArrowLeft = false;
-                    break;
-                case 'ArrowDown':
-                    this.key_state.ArrowDown = false;
-                    break;
-                case 'ArrowRight':
-                    this.key_state.ArrowRight = false;
-                    break;
-            }
-            if (event.key === 'Enter' && this.isEnterPressed && this.isBallStationary()) {
-                this.isEnterPressed = false;
-                this.ball_velocity = this.aim_direction.times(this.potentialVelocity);
-                this.enter_release_time = Date.now();
-                this.potentialVelocity = 0;
-                this.strokes += 1;
-                event.preventDefault();
-            }
-        });
-        
     }
 
     make_control_panel() {
@@ -548,9 +490,6 @@ export class Main extends Scene {
             } else {
                 this.game_completed = true;
                 this.display_win_screen();
-                // alert("You have completed all levels!");
-                // this.current_level = 0;  // Reset to the first level or end game
-                // this.init_level(this.current_level);  // Restart at the first level or display end game screen
             }
         }
     }
@@ -572,11 +511,6 @@ export class Main extends Scene {
     }
     display(context, program_state) {
         this.current_program_state = program_state;
-
-        if (!context.scratchpad.controls) {
-            this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
-            program_state.set_camera(this.initial_camera_location);
-        }
 
         program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 0.1, 100);
 
@@ -683,111 +617,5 @@ export class Main extends Scene {
     }
 
         this.shapes.hole.draw(context, program_state, hole_transform, this.materials.hole);
-    }
-}
-
-class Gouraud_Shader extends Shader {
-    constructor(num_lights = 2) {
-        super();
-        this.num_lights = num_lights;
-    }
-
-    shared_glsl_code() {
-        return ` 
-        precision mediump float;
-        const int N_LIGHTS = ` + this.num_lights + `;
-        uniform float ambient, diffusivity, specularity, smoothness;
-        uniform vec4 light_positions_or_vectors[N_LIGHTS], light_colors[N_LIGHTS];
-        uniform float light_attenuation_factors[N_LIGHTS];
-        uniform vec4 shape_color;
-        uniform vec3 squared_scale, camera_center;
-
-        varying vec3 N, vertex_worldspace;
-        varying vec4 vertex_color;
-
-        vec3 phong_model_lights( vec3 N, vec3 vertex_worldspace ){                                        
-            vec3 E = normalize( camera_center - vertex_worldspace );
-            vec3 result = vec3( 0.0 );
-            for(int i = 0; i < N_LIGHTS; i++){
-                vec3 surface_to_light_vector = light_positions_or_vectors[i].xyz - 
-                                               light_positions_or_vectors[i].w * vertex_worldspace;                                             
-                float distance_to_light = length( surface_to_light_vector );
-
-                vec3 L = normalize( surface_to_light_vector );
-                vec3 H = normalize( L + E );
-                float diffuse  =      max( dot( N, L ), 0.0 );
-                float specular = pow( max( dot( N, H ), 0.0 ), smoothness );
-                float attenuation = 1.0 / (1.0 + light_attenuation_factors[i] * distance_to_light * distance_to_light );
-                
-                vec3 light_contribution = shape_color.xyz * light_colors[i].xyz * diffusivity * diffuse
-                                                          + light_colors[i].xyz * specularity * specular;
-                result += attenuation * light_contribution;
-            }
-            return result;
-        } `;
-    }
-
-    vertex_glsl_code() {
-        return this.shared_glsl_code() + `
-            attribute vec3 position, normal;                            
-            uniform mat4 model_transform;
-            uniform mat4 projection_camera_model_transform;
-    
-            void main(){                                                                   
-                gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
-                N = normalize( mat3( model_transform ) * normal / squared_scale);
-                vertex_worldspace = ( model_transform * vec4( position, 1.0 ) ).xyz;
-
-                vertex_color = vec4(shape_color.xyz * ambient, shape_color.w);
-                vertex_color.xyz += phong_model_lights (normalize(N), vertex_worldspace);
-            } `;
-    }
-
-    fragment_glsl_code() {
-        return this.shared_glsl_code() + `
-            void main(){
-                gl_FragColor = vertex_color;
-            } `;
-    }
-
-    send_material(gl, gpu, material) {
-        gl.uniform4fv(gpu.shape_color, material.color);
-        gl.uniform1f(gpu.ambient, material.ambient);
-        gl.uniform1f(gpu.diffusivity, material.diffusivity);
-        gl.uniform1f(gpu.specularity, material.specularity);
-        gl.uniform1f(gpu.smoothness, material.smoothness);
-    }
-
-    send_gpu_state(gl, gpu, gpu_state, model_transform) {
-        const O = vec4(0, 0, 0, 1), camera_center = gpu_state.camera_transform.times(O).to3();
-        gl.uniform3fv(gpu.camera_center, camera_center);
-        const squared_scale = model_transform.reduce(
-            (acc, r) => {
-                return acc.plus(vec4(...r).times_pairwise(r))
-            }, vec4(0, 0, 0, 0)).to3();
-        gl.uniform3fv(gpu.squared_scale, squared_scale);
-        const PCM = gpu_state.projection_transform.times(gpu_state.camera_inverse).times(model_transform);
-        gl.uniformMatrix4fv(gpu.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
-        gl.uniformMatrix4fv(gpu.projection_camera_model_transform, false, Matrix.flatten_2D_to_1D(PCM.transposed()));
-
-        if (!gpu_state.lights.length)
-            return;
-
-        const light_positions_flattened = [], light_colors_flattened = [];
-        for (let i = 0; i < 4 * gpu_state.lights.length; i++) {
-            light_positions_flattened.push(gpu_state.lights[Math.floor(i / 4)].position[i % 4]);
-            light_colors_flattened.push(gpu_state.lights[Math.floor(i / 4)].color[i % 4]);
-        }
-        gl.uniform4fv(gpu.light_positions_or_vectors, light_positions_flattened);
-        gl.uniform4fv(gpu.light_colors, light_colors_flattened);
-        gl.uniform1fv(gpu.light_attenuation_factors, gpu_state.lights.map(l => l.attenuation));
-    }
-
-    update_GPU(context, gpu_addresses, gpu_state, model_transform, material) {
-        const defaults = { color: color(0, 0, 0, 1), ambient: 0, diffusivity: 1, specularity: 1, smoothness: 40 };
-        material = Object.assign({}, defaults, material);
-
-        this.send_material(context, gpu_addresses, material);
-        this.send_gpu_state(context, gpu_addresses, gpu_state, model_transform);
     }
 }
